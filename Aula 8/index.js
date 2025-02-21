a2ed73eb148821cd10f4b294ae208bd142f297da
Ramon <ramonempresa2001@gmail.com>
2025-02-20 22:32:34 -0300

    exercise1

--- Aula 8/index.js
+++ Aula 8/index.js
@@ -1,1 +1,14 @@
-console.log("Olá, mundo!")
+const nome = "Ramon";
+const age = 24;
+const height = 1.85;
+const weight = 80;
+
+let imc = weight / (height ^ 2);
+
+let born = 2025 - age;
+
+console.log("Seu nome é", nome, "tem", age, "anos, pesa", weight, "kg");
+
+console.log("Tem", height, "de altura e seu IMC é de", imc);
+
+console.log(nome, "nasceu no ano de", born);
