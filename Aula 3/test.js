b2a00ac0cc2ab6ccc5894ee89c015dc67ab68e1a
Ramon <ramonempresa2001@gmail.com>
2025-02-28 23:07:47 -0300

    notesToMyself1.1

--- Aula 3/test.js
+++
@@ -1,3 +0,0 @@
-console.log("Meu nome é Ramon. Estou aprendendo JavaScript às", 7, "da manhã, irei conseguir a minha vaga de emprego em dólares!")
-
-console.log("In the next 3 years, I'll be an IA engineer.")
