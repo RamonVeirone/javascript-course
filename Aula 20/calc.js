fa0386d7ed9f969d67ad4f356915fea28c8b91b5
Ramon <ramonempresa2001@gmail.com>
2025-03-02 21:43:58 -0300

    exercise8.3.1_functions

--- Aula 20/calc.js
+++ Aula 20/calc.js
@@ -1,7 +1,43 @@
-const form = document.querySelector('#formulario');
+const form = document.querySelector("#formulario");
 
-form.addEventListener('submit', function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("Evento previnido");
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+
+  if (!altura) {
+    setResultado("Altura inválido", false);
+    return;
+  }
+
+  const imc = getIMC(peso, altura);
+
+  console.log(imc);
 });
 
+function getIMC() {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+
+  const p = criaP();
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
