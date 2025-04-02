84a71a6ec1717a141c1ac4d355705256fde8b47a
Ramon <ramonempresa2001@gmail.com>
2025-04-02 20:10:49 -0300

    treino12

--- Treino/treino.js
+++ Treino/treino.js
@@ -1,1 +1,8 @@
-console.log("Olá, mundo!")
+const numbers = [10, 20, 30, 40, 50, 60, 70];
+
+
+const numbersInDobro = numbers.map(function(number) {
+    return number *3;
+});
+
+console.log(numbersInDobro)
