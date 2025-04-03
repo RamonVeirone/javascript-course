b10128eaeac2b613ff80f823a7b23932390a24a9
Ramon <ramonempresa2001@gmail.com>
2025-04-02 21:07:44 -0300

    treino13

--- Treino/treino.js
+++ Treino/treino.js
@@ -1,8 +1,22 @@
-const numbers = [10, 20, 30, 40, 50, 60, 70];
+const numbers = [10, 20, 30, 45, 50, 60, 75];
 
 
 const numbersInDobro = numbers.map(function(number) {
     return number *3;
 });
 
-console.log(numbersInDobro)
+//console.log(numbersInDobro)
+
+const ages = [16, 17, 18, 19, 22, 24];
+
+const bestAges = ages.filter(function(ages){
+    return ages % 2 === 0
+})
+
+//console.log(bestAges)
+
+const reducedNumbers = numbers.reduce(function(numbers, accumulator){
+    return accumulator + numbers
+}, 0)
+
+//console.log(reducedNumbers)
