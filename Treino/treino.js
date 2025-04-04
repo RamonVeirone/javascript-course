1e88803a17158f83589bc3cda558f97e8b1664a3
Ramon <ramonempresa2001@gmail.com>
2025-04-04 20:16:00 -0300

    treino19

--- Treino/treino.js
+++ Treino/treino.js
@@ -4,7 +4,7 @@ const numbersInDobro = numbers.map(function (number) {
   return number * 3;
 });
 
-//console.log(numbersInDobro)
+// console.log(numbersInDobro)
 
 const ages = [16, 17, 18, 19, 22, 24];
 
@@ -12,13 +12,13 @@ const bestAges = ages.filter(function (ages) {
   return ages % 2 === 0;
 });
 
-//console.log(bestAges)
+// console.log(bestAges)
 
 const reducedNumbers = numbers.reduce(function (numbers, accumulator) {
   return accumulator + numbers;
 }, 0);
 
-//console.log(reducedNumbers)
+// console.log(reducedNumbers)
 
 const person = {
   firstName: "Ramon",
@@ -32,16 +32,32 @@ const personJson = JSON.stringify(person); // comando pra enviar para para servi
 
 // console.log(personJson)
 
-//for (let index = 1; index <= 10; index++) {
+// for (let index = 1; index <= 10; index++) {
 //  console.log(index);}
 
 const carros = ["BMW", "Audi", "Mercedes"];
 
-//for (let i = 0; i < carros.length; i++) {
+// for (let i = 0; i < carros.length; i++) {
 //  console.log(carros[i]);}
 
 function soma(x, y) {
   return x + y;
 }
 
-console.log(soma(5,8));
+//console.log(soma(5,8));
+
+class Pessoa {
+  construtor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  getFullName(){
+    console.log (`${this.firstName} + ${this.lastName}`);
+  }
+}
+
+const novapessoa = new Pessoa("Ramon", "Alves", 23);
+
+console.log(novapessoa);
